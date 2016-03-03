b8c12569e8773d1d742f17d0ff7a1fb43db11a29
Ophir LOJKINE <pere.jobs@gmail.com>
2016-03-03 09:09:00 +0300

    Set custom SSL options in the proxy
    The goal is to get the resource, there is no security concern here.
    Our host at alwaysdata.net seems to have PHP compiled with an old
    version of OpenSSL

--- proxy.php
+++ proxy.php
@@ -8,6 +8,11 @@ $opts = array(
     'header'=>"User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36\r\n" .
               "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" .
               "Accept-Language: en-US,en;q=0.5"
+    ),
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+        "crypto_method" => STREAM_CRYPTO_METHOD_TLSv1_0_CLIENT
     )
 );
 $context = stream_context_create($opts);
