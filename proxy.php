c5ada442682ba73966b72807bbec2532ad2b9fdf
Ophir LOJKINE <pere.jobs@gmail.com>
2016-04-12 23:03:57 +0300

    Add missing line feed in request headers

--- proxy.php
+++ proxy.php
@@ -6,7 +6,7 @@ if (strpos($url, "http") !== 0) die("Only http requests are allowed.");
 $opts = array(
   'http'=>array(
     'header'=>"User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36\r\n" .
-              "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" .
+              "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" .
               "Accept-Language: en-US,en;q=0.5"
     ),
     "ssl"=>array(
