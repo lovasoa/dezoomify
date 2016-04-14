9a7e1a250e7813bbe3d7d896969758cd97b3dd6d
Ophir LOJKINE <pere.jobs@gmail.com>
2016-04-14 03:33:31 +0300

    Rework tests

---
+++ tests/karma.conf.js
@@ -0,0 +1,84 @@
+// Karma configuration
+// Generated on Thu Apr 14 2016 00:59:07 GMT+0300 (MSK)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '..',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['qunit'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      {pattern:'dezoomify.html', included:false},
+      {pattern:'style.css', included:false},
+      {pattern:'zoommanager.js', included:false},
+      {pattern:'dezoomers/*js', included:false},
+      'tests/test_urls.js',
+      'tests/karma-tests.js',
+    ],
+
+    // proxies
+    proxies: {
+      "/dezoomify/": "../"
+    },
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Firefox'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+    client: {
+      qunit: {
+        autostart: false
+      }
+    }
+  })
+}
--- tests/tests.js
+++ tests/tests.js
@@ -1,47 +1,64 @@
-QUnit.config.autostart = false;
+if (typeof BASE === "undefined") BASE = "/base";
 
-var testwin = document.getElementById("dezoomify").contentWindow;
+QUnit.module("Image loads", {
+  beforeEach: function (assert) {
+    var done = assert.async();
+    var that = this;
 
-testwin.onload = function() {
-  var testdoc = testwin.document;
-  var ZoomManager = testwin.ZoomManager;
-  // Execute the tests faster: don't wait between fake tile loads
-  ZoomManager.nextTick = function(f) {return setTimeout(f,0);};
-  var UI = testwin.UI;
+    var iframe = document.createElement("iframe");
+    iframe.src = BASE + "/dezoomify.html";
+    document.body.appendChild(iframe);
 
-  test_urls.forEach(function(test) {
-    QUnit.test(test.name, function( assert ) {
-      assert.expect(1);
-      var done = assert.async();
+    var testwin = iframe.contentWindow;
+
+    testwin.onload = function() {
+      var testdoc = testwin.document;
+      var ZoomManager = testwin.ZoomManager;
+      // Execute the tests faster: don't wait between fake tile loads
+      ZoomManager.nextTick = function(f) {return setTimeout(f,0);};
+      ZoomManager.proxy_url = "http://127.0.0.1:8181/proxy.php";
+      that.ZoomManager = ZoomManager;
+      that.UI = testwin.UI;
+      that.testwin = testwin;
+      done();
+    }
+  }
+});
+
+test_urls.forEach(function(test) {
+  QUnit.test(test.name, function( assert ) {
+    var ZoomManager = this.ZoomManager,
+        UI = this.UI,
+        testwin = this.testwin;
+    assert.expect(1);
+    var done = assert.async();
+    ZoomManager.reset();
+    ZoomManager.open(test.url);
+    ZoomManager.loadEnd = function () {
       ZoomManager.reset();
-      ZoomManager.open(test.url);
-      ZoomManager.loadEnd = function () {
-        ZoomManager.reset();
-        assert.ok(true, "Image loaded")
-        done();
+      assert.ok(true, "Image loaded")
+      done();
+    }
+    ZoomManager.addTile = function (url, x, y) {
+      //In order to save time & bandwidth, load only the last tile
+      if (x*y === (ZoomManager.data.nbrTilesX-1) *
+                  (ZoomManager.data.nbrTilesY-1) *
+                  (ZoomManager.data.tileSize * ZoomManager.data.tileSize)) {
+          var img = document.createElement("img");
+          img.onload = function () {ZoomManager.status.loaded ++;};
+          img.onerror = function() {
+            assert.ok(false, "Invalid tile image: " + url);
+            done();
+          }
+          img.src = url;
+      } else {
+        ZoomManager.status.loaded ++;
+        UI.canvas.width = UI.canvas.height = UI.canvas.style.width = UI.canvas.style.height = 0;
       }
-      ZoomManager.addTile = function (url, x, y) {
-      	//In order to save time & bandwidth, load only the last tile
-        if (x*y === (ZoomManager.data.nbrTilesX-1) *
-                    (ZoomManager.data.nbrTilesY-1) *
-                    (ZoomManager.data.tileSize * ZoomManager.data.tileSize)) {
-            var img = document.createElement("img");
-            img.onload = function () {ZoomManager.status.loaded ++;};
-            img.onerror = function() {
-              assert.ok(false, "Invalid tile image: " + url);
-              done();
-            }
-            img.src = url;
-        } else {
-          ZoomManager.status.loaded ++;
-          UI.canvas.width = UI.canvas.height = UI.canvas.style.width = UI.canvas.style.height = 0;
-        }
-      };
-      testwin.onerror = function(err, source, lineno) {
-        assert.ok(false, "Dezoomify bug " + err + "\n" + source + ':' + lineno);
-        done();
-      }
-    });
+    };
+    testwin.onerror = function(err, source, lineno) {
+      assert.ok(false, "Dezoomify bug " + err + "\n" + source + ':' + lineno);
+      done();
+    }
   });
-  QUnit.start();
-}
+});
