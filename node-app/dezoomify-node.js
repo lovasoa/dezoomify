41b99c5785b83741ad7a3208aeb31d2c86c96219
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2020-01-04 20:49:48 +0100

    Reformat the code of the node app

--- node-app/dezoomify-node.js
+++ node-app/dezoomify-node.js
@@ -33,32 +33,32 @@ function onload(window) {
     console.log("Saved the image to " + target_filename);
     proxy_server.close();
   }
-  UI.updateProgress = function(progress, text) {
+  UI.updateProgress = function (progress, text) {
     console.log(parseInt(progress) + "% : " + text);
   }
   UI.setupRendering = function (data) {
-  	UI.canvas = new Canvas.Canvas(data.width, data.height);
-  	UI.ctx = UI.canvas.getContext("2d");
+    UI.canvas = new Canvas.Canvas(data.width, data.height);
+    UI.ctx = UI.canvas.getContext("2d");
   };
-  ZoomManager.addTile = function addTile (url, x, y, nTries) {
+  ZoomManager.addTile = function addTile(url, x, y, nTries) {
     if (nTries === (void 0)) nTries = 10;
     //Request a tile from the server, and prints add it to the canvas when it's received
-    request({url, encoding:null}, function tileLoaded(err, stream, buffer){
+    request({ url, encoding: null }, function tileLoaded(err, stream, buffer) {
       if (err) {
         if (nTries === 0) {
           return ZoomManager.error("Error while loading tile: " + url + "\n" + err);
         } else {
           console.log("Request failed, retrying :" + nTries);
-          return setTimeout(addTile, Math.pow(2, 10-nTries), url, x, y, nTries-1);
+          return setTimeout(addTile, Math.pow(2, 10 - nTries), url, x, y, nTries - 1);
         }
       }
       var img = new Canvas.Image;
       img.src = buffer;
       UI.drawTile(img, x, y);
-      ZoomManager.status.loaded ++;
+      ZoomManager.status.loaded++;
     });
   };
-  ZoomManager.proxy_url = "http://127.0.0.1:"+PROXY_PORT;
+  ZoomManager.proxy_url = "http://127.0.0.1:" + PROXY_PORT;
   ZoomManager.open(target_url);
 }
 
@@ -66,6 +66,6 @@ jsdom.JSDOM.fromFile(path.join(DEZOOMIFY_PATH, "dezoomify.html"), {
   virtualConsole,
   runScripts: "dangerously",
   resources: "usable",
-}).then(function(dom){
+}).then(function (dom) {
   dom.window.onload = onload.bind(null, dom.window)
 });
