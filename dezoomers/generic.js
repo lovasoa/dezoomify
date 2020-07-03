ebba8a0aa2fa727754b5af20bdfc6e97b7cd70e2
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2020-07-03 11:48:44 +0200

    Add support for padding in the generic dezoomer
    Fixes #413
    Fixes #382

--- dezoomers/generic.js
+++ dezoomers/generic.js
@@ -1,23 +1,35 @@
-var generic_viewer = (function(){
+var generic_viewer = (function () {
+  var coordinateRegex = /\{\{([XY])(?::(.)(\d+))?\}\}/gi;
+  function fillTemplate(tpl, coords) {
+    return tpl.replace(coordinateRegex, function (base, coord, padding, widthStr) {
+      var n = coords[coord.toLowerCase()];
+      var width = parseInt(widthStr);
+      var result = n.toString();
+      while (result.length < width) {
+        result = padding + result;
+      }
+      return result;
+    });
+  }
   return {
-    "name" : "Generic dezoomer",
+    "name": "Generic dezoomer",
     "description": "Just put the url of a tile, replacing it's horizontal coordinate by {{X}} and vertical coordinate by {{Y}}.",
-    "urls" : [
-      /\{\{X\}\}/
+    "urls": [
+      coordinateRegex
     ],
-    "open" : function (url) {
+    "open": function (url) {
       var current_dimension = 0;
-      var dimensions_interval = [[0,1000], [0,1000]];
+      var dimensions_interval = [[0, 1000], [0, 1000]];
       var tileSize = 256;
       function middle(interval) {
         return Math.floor((interval[0] + interval[1]) / 2);
       }
       function dichotomy_step() {
         var img = new Image;
-        var coords = dimensions_interval.map(function(interval, i){
+        var coords = dimensions_interval.map(function (interval, i) {
           return i === current_dimension ? middle(interval) : 0;
         });
-        img.src = url.replace("{{X}}", coords[0]).replace("{{Y}}", coords[1]);
+        img.src = fillTemplate(url, { x: coords[0], y: coords[1] });
         function next_image(border) {
           var interval = dimensions_interval[current_dimension];
           var new_coord = middle(interval);
@@ -25,13 +37,13 @@ var generic_viewer = (function(){
             interval[border] = new_coord;
             console.log(dimensions_interval);
             if (interval[0] + 1 == interval[1])
-              current_dimension ++;
+              current_dimension++;
             if (current_dimension >= dimensions_interval.length) {
               return ZoomManager.readyToRender({
-                "origin" : url,
-                "width" : dimensions_interval[0][1]*tileSize,
-                "height" : dimensions_interval[1][1]*tileSize,
-                "tileSize" : tileSize
+                "origin": url,
+                "width": dimensions_interval[0][1] * tileSize,
+                "height": dimensions_interval[1][1] * tileSize,
+                "tileSize": tileSize
               });
             }
             if (img.height > 0 && img.width > 0) {
@@ -49,8 +61,8 @@ var generic_viewer = (function(){
       }
       return dichotomy_step();
     },
-    "getTileURL" : function (x, y, zoom, data) {
-      return data.origin.replace("{{X}}", x).replace("{{Y}}", y);
+    "getTileURL": function (x, y, zoom, data) {
+      return fillTemplate(data.origin, { x: x, y: y });
     }
   };
 })();
