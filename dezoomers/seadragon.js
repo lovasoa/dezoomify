7cf529546e95b2f452e1fd7eb6d3fd10836875be
ophir <pere.jobs@gmail.com>
2020-07-18 15:14:44 +0200

    fix bibliotheques-specialisees.paris.fr

--- dezoomers/seadragon.js
+++ dezoomers/seadragon.js
@@ -1,9 +1,9 @@
 var seadragon = (function () { //Code isolation
 
 	return {
-		"name" : "Seadragon (Deep Zoom Image)",
-		"description" : "Microsoft zoomable image format, sometimes called DZI, seadragon, or deep zoom",
-		"urls" : [
+		"name": "Seadragon (Deep Zoom Image)",
+		"description": "Microsoft zoomable image format, sometimes called DZI, seadragon, or deep zoom",
+		"urls": [
 			/bl\.uk\/manuscripts\/Viewer\.aspx/,
 			/polona\.pl\/item\//,
 			/bibliotheques-specialisees\.paris\.fr\/ark/,
@@ -11,14 +11,14 @@ var seadragon = (function () { //Code isolation
 			/_files\/\d+\/\d+_\d+.jpg$/,
 			/dzi$/
 		],
-		"contents" : [
+		"contents": [
 			/dziUrlTemplate/,
 			/[^"'()<>]+\.(?:dzi)/i,
 			/schemas\.microsoft\.com\/deepzoom/,
 			/zoom(?:\.it|hub.net)\/.*\.js/,
 			/\b(dzi|DZI)\b/
 		],
-		"findFile" : function getDZIFile (baseUrl, callback) {
+		"findFile": function getDZIFile(baseUrl, callback) {
 			if (baseUrl.match(/\.xml|\.dzi/i)) {
 				return callback(baseUrl);
 			}
@@ -26,16 +26,25 @@ var seadragon = (function () { //Code isolation
 			// British Library
 			if (baseUrl.indexOf("bl.uk/manuscripts/Viewer.aspx") > -1) {
 				return callback(baseUrl
-													.replace("Viewer.aspx","Proxy.ashx")
-													.replace(/ref=([^&]*)/, "view=$1.xml"));
+					.replace("Viewer.aspx", "Proxy.ashx")
+					.replace(/ref=([^&]*)/, "view=$1.xml"));
+			}
+
+			var parisMatch = baseUrl.match(/bibliotheques-specialisees.paris.fr\/ark:((?:\/\w+){3})(.*)/);
+			if (parisMatch) {
+				baseUrl = "https://bibliotheques-specialisees.paris.fr/in/imageReader.xhtml" +
+					"?id=ark:" + parisMatch[1] +
+					"&updateUrl=updateUrl1653" +
+					"&ark=" + parisMatch[1] + parisMatch[2] +
+					"&selectedTab=otherdocs";
 			}
 
 			// Polona.pl
 			var polonaMatch = baseUrl.match(/polona.pl\/item\/(\d+)\/(\d+)/);
 			if (polonaMatch) {
 				var itemId = polonaMatch[1], pageId = parseInt(polonaMatch[2]);
-				var resUrl = "http://polona.pl/resources/item/"+itemId+"/?format=json";
-				ZoomManager.getFile(resUrl, {type:"json"}, function(res, xhr) {
+				var resUrl = "http://polona.pl/resources/item/" + itemId + "/?format=json";
+				ZoomManager.getFile(resUrl, { type: "json" }, function (res, xhr) {
 					callback(res.pages[pageId].dzi_url);
 				});
 				return;
@@ -50,7 +59,7 @@ var seadragon = (function () { //Code isolation
 			var tileMatch = baseUrl.match(/(.*)_files\/\d+\/\d+_\d+.jpg$/);
 			if (tileMatch) return callback(tileMatch[1] + ".dzi");
 
-			ZoomManager.getFile(baseUrl, {type:"htmltext"}, function (text, xhr) {
+			ZoomManager.getFile(baseUrl, { type: "htmltext" }, function (text, xhr) {
 				// World digital library
 				var wdlMatch = baseUrl.match(/view\/(\d+)\/(\d+)/);
 				if (wdlMatch && text.match("dziUrlTemplate")) {
@@ -67,10 +76,6 @@ var seadragon = (function () { //Code isolation
 					return callback("http://content.zoomhub.net/dzis/" + zoomitMatch[1] + ".dzi");
 				}
 
-				// bibliothèques specialisées de la ville de Paris
-				var parisMatch = text.match(/deepZoomManifest['"]\s*:\s*["']([^"']*)/);
-				if (parisMatch) return callback(parisMatch[1]);
-
 				// Any url ending with .xml or .dzi
 				var matchPath = text.match(/[^"'()<>]+\.(?:xml|dzi)/i);
 				if (matchPath) return callback(matchPath[0]);
@@ -82,8 +87,8 @@ var seadragon = (function () { //Code isolation
 				return callback(baseUrl);
 			});
 		},
-		"open" : function (url) {
-			ZoomManager.getFile(url, {type:"xml"}, function (xml, xhr) {
+		"open": function (url) {
+			ZoomManager.getFile(url, { type: "xml" }, function (xml, xhr) {
 				var infos = xml.getElementsByTagName("Image")[0];
 				var size = xml.getElementsByTagName("Size")[0];
 				if (!infos || !size) return ZoomManager.error("Invalid seadragon XML info file: " + url);
@@ -94,7 +99,7 @@ var seadragon = (function () { //Code isolation
 					data.origin = url + "?tile=";
 				} else {
 					//replace extension by _files
-					data.origin = url.replace(/\.[^.\/]*$/,'') + "_files/";
+					data.origin = url.replace(/\.[^.\/]*$/, '') + "_files/";
 				}
 				data.tileSize = parseInt(infos.getAttribute("TileSize"));
 				data.overlap = parseInt(infos.getAttribute("Overlap"));
@@ -107,12 +112,12 @@ var seadragon = (function () { //Code isolation
 				data.zoomFactor = 2;
 				// 2^maxzoom = max(w,h) (the first tile is 1x1)
 				data.maxZoomLevel = Math.ceil(
-						Math.log2(Math.max(data.width, data.height))
+					Math.log2(Math.max(data.width, data.height))
 				);
 				ZoomManager.readyToRender(data);
 			});
 		},
-		"getTileURL" : function (col, row, zoom, data) {
+		"getTileURL": function (col, row, zoom, data) {
 			return data.origin + zoom + "/" + col + "_" + row + "." + data.format;
 		}
 	};
