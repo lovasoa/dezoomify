2489ae74a9d5068b0f34a78a485a1e76cb8f8483
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2020-09-27 20:21:24 +0200

    Handle HTTP redirections in the cloudflare worker

--- cloudflare-worker-proxy.js
+++ cloudflare-worker-proxy.js
@@ -2,19 +2,30 @@
  * This is a cloudflare worker for dezoomify
  */
 
+const MAX_REDIRECT = 3;
+
 /**
  * Respond to the request
  * @param {Request} request
  */
 async function handleRequest(request) {
     const url = new URL(request.url);
-    const target_url = new URL(url.searchParams.get("url"));
+    let target_url = new URL(url.searchParams.get("url"));
     let target_request = new Request(target_url, request);
     target_request.headers.set("Origin", target_url.origin);
     target_request.headers.set("Referer", target_url.toString());
     const cookies = url.searchParams.get("cookies");
     if (cookies) target_request.headers.set("Cookie", cookies);
     let response = await fetch(target_request);
+    let location = response.headers.get("Location");
+    for (let i = 0; i < MAX_REDIRECT && location; i++) {
+        response = await fetch(location, target_request);
+        location = response.headers.get("Location");
+    }
+    if (location) {
+        response.headers.set("X-Disabled-Location", location);
+        response.headers.set("Location", "");
+    }
     response = new Response(response.body, response);
     const response_cookie = response.headers.get("Set-Cookie");
     if (response_cookie) response.headers.set("X-Set-Cookie", response_cookie);
