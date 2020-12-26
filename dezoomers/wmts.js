9182e407446f12f8ff8f28c3b6c274ca37462a52
lovasoa <pere.jobs@gmail.com>
2020-12-26 11:00:38 +0100

    progress on wmts

--- dezoomers/wmts.js
+++ dezoomers/wmts.js
@@ -9,6 +9,7 @@ function parseCoordinates(text) {
     .map((s) => parseFloat(s));
 }
 
+// See the standard: https://www.ogc.org/standards/wmts
 ZoomManager.addDezoomer({
   name: "WMTS",
   description: "OpenGIS Web Map Tile Service Implementation Standard",
@@ -25,7 +26,7 @@ ZoomManager.addDezoomer({
         .getElementsByTagName("ResourceURL")[0]
         .getAttribute("template");
       var bboxEl = layer.getElementsByTagName("ows:WGS84BoundingBox")[0];
-      const [[minx, maxy], [maxx, miny]] = [
+      const [[bboxMinX, bboxMinY], [bboxMaxX, bboxMaxY]] = [
         "ows:LowerCorner",
         "ows:UpperCorner",
       ].map((name) =>
@@ -39,32 +40,53 @@ ZoomManager.addDezoomer({
       const tileMatrices = Array.from(
         tileMatrixSet.getElementsByTagName("TileMatrix")
       ).map((m) => {
-        const tileSize = parseInt(
+        const tileWidth = parseInt(
           m.getElementsByTagName("TileWidth")[0].textContent
         );
-        const matrix_width = parseInt(
+        const tileHeight = parseInt(
+          m.getElementsByTagName("TileHeight")[0].textContent
+        );
+        const matrixWidth = parseInt(
           m.getElementsByTagName("MatrixWidth")[0].textContent
         );
-        const matrix_height = parseInt(
+        const matrixHeight = parseInt(
           m.getElementsByTagName("MatrixHeight")[0].textContent
         );
         const identifier = String(
           m.getElementsByTagName("ows:Identifier")[0].textContent
         );
-        const scale = parseFloat(
+        const scaleDenominator = parseFloat(
           m.getElementsByTagName("ScaleDenominator")[0].textContent
         );
-        const topLeft = parseCoordinates(
+        const [tileMatrixMaxY, tileMatrixMinX] = parseCoordinates(
           m.getElementsByTagName("TopLeftCorner")[0].textContent
         );
+        const metersPerUnit = (6378137 * Math.PI) / 180;
+        const pixelSpan = scaleDenominator * 0.28e-3 / metersPerUnit;
+        const tileSpanX = tileWidth * pixelSpan;
+        const tileSpanY = tileHeight * pixelSpan;
+        const epsilon = 1e-6;
+        const tileMinCol = Math.floor(
+          (bboxMinX - tileMatrixMinX) / tileSpanX + epsilon
+        );
+        const tileMaxCol = Math.floor(
+          (bboxMaxX - tileMatrixMinX) / tileSpanX - epsilon
+        );
+        const tileMinRow = Math.floor(
+          (tileMatrixMaxY - bboxMaxY) / tileSpanY + epsilon
+        );
+        const tileMaxRow = Math.floor(
+          (tileMatrixMaxY - bboxMinY) / tileSpanY - epsilon
+        );
         return {
-          tileSize,
-          width: matrix_width * tileSize,
-          height: matrix_height * tileSize,
+          tileSize: tileWidth,
+          width: tileMaxCol - tileMinCol,
+          height: tileMaxRow - tileMinRow,
           identifier,
-          scale,
-          corners,
-          topLeft,
+          tileMinCol,
+          tileMinRow,
+          tileMaxCol,
+          tileMaxRow,
         };
       });
       const tileMatrix = tileMatrices.sort((a, b) => a.scale - b.scale)[0];
